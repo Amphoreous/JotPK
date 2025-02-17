b394e70629770ece7c3cf7255e198d841417d2b7
TheUnrealZaka <info@theunrealzaka.me>
2025-02-17 08:58:12 +0100

    Arreglao

--- src/main.c
+++ src/main.c
@@ -1,104 +1,21 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
 #include "raylib.h"
+#include "resource_dir.h"
 
-<<<<<<< Updated upstream
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
-=======
 typedef enum { MENU, GAME, HTP } GameScreen;
->>>>>>> Stashed changes
 
-//int main ()
-//{
-//	// Tell the window to use vsync and work on high DPI displays
-//	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-//
-//	// Create the window and OpenGL context
-//	InitWindow(1280, 800, "Hello Raylib");
-//
-//	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-//	SearchAndSetResourceDir("resources");
-//
-//	// Load a texture from the resources directory
-//	Texture wabbit = LoadTexture("wabbit_alpha.png");
-//	
-//	// game loop
-//	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
-//	{
-//		// drawing
-//		BeginDrawing();
-//
-//		// Setup the back buffer for drawing (clear color and depth buffers)
-//		ClearBackground(BLACK);
-//
-//		// draw some text using the default font
-//		DrawText("Hello Raylib", 200,200,20,WHITE);
-//
-//		// draw our texture to the screen
-//		DrawTexture(wabbit, 400, 200, WHITE);
-//		
-//		// end the frame and get ready for the next one  (display frame, poll input, etc...)
-//		EndDrawing();
-//	}
-//
-//	// cleanup
-//	// unload our texture so it can be cleaned up
-//	UnloadTexture(wabbit);
-//
-//	// destroy the window and cleanup the OpenGL context
-//	CloseWindow();
-//	return 0;
-//}
-
-int main(void) 
+int main(void)
 {
-	// Initialization
-	SetConfigFlags(FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_MAXIMIZED); // Set window configuration state using flags
-    InitWindow(GetScreenWidth(), GetScreenHeight(), "Journey of the Praire King"); // Initialize window and OpenGL context
-
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+    SetConfigFlags(FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_MAXIMIZED);
+    InitWindow(GetScreenWidth(), GetScreenHeight(), "Journey of the Prairie King");
+    SetTargetFPS(60);
     SearchAndSetResourceDir("resources");
+    Texture JotPKLogo = LoadTexture("JotPK_logo.png");
 
-    // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
+    GameScreen currentScreen = MENU;
+    int selectedOption = 0;
 
-    Image image = LoadImage("JotPK.png");     // Loaded in CPU memory (RAM)
-    Texture2D texture = LoadTextureFromImage(image);          // Image converted to texture, GPU memory (VRAM)
-    UnloadImage(image);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
-
-
-
-
-    //Para que el juego vaya a 60 FPS
-
-    SetTargetFPS(60);       
-    
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-<<<<<<< Updated upstream
-        // Update
-   
-        // TODO: Update your variables here
-
-        // Draw
-
-        BeginDrawing();
-
-        ClearBackground(BLACK);
-
-        DrawTexture(texture, 200 / 2 - texture.width / 2, 200 / 2 - texture.height / 2, WHITE);
-
-        EndDrawing();
-
-=======
         if (currentScreen == MENU)
         {
             if (IsKeyPressed(KEY_DOWN)) selectedOption = (selectedOption + 1) % 3;
@@ -108,16 +25,15 @@ int main(void)
                 if (selectedOption == 0) currentScreen = GAME; // Iniciar juego
                 else if (selectedOption == 1) // Configuración
                 {
-                
-           
-                } 
-                else if (selectedOption == 2) 
+
+
+                }
+                else if (selectedOption == 2)
                 {
                     currentScreen = HTP;
                 }
                 else if (selectedOption == 3) break; // Salir
             }
-
             BeginDrawing();
             ClearBackground(BLACK);
             DrawTexture(JotPKLogo, (GetScreenWidth() - JotPKLogo.width) / 2, 50, WHITE);
@@ -139,20 +55,11 @@ int main(void)
             BeginDrawing();
             DrawText("HOW TO PLAY", GetScreenWidth() / 2, 50, WHITE);
             DrawText("Sasdasdadassdad", 190, 300, 20, WHITE);
-
             EndDrawing();
         }
->>>>>>> Stashed changes
     }
 
-    // De-Initialization 
-    
-    //Cierra la ventana y abre OpenGL context
-
-    UnloadTexture(texture);
-
-    CloseWindow();  
-
-
+    UnloadTexture(JotPKLogo);
+    CloseWindow();
     return 0;
 }
