a3f5c8aeff3f72fe084dde6094febe425f7c1a19
Katy <sofiagiva1@gmail.com>
2025-02-17 08:51:20 +0100

    Update main.c
    uwu

--- src/main.c
+++ src/main.c
@@ -9,7 +9,11 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "raylib.h"
 
+<<<<<<< Updated upstream
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+=======
+typedef enum { MENU, GAME, HTP } GameScreen;
+>>>>>>> Stashed changes
 
 //int main ()
 //{
@@ -79,6 +83,7 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+<<<<<<< Updated upstream
         // Update
    
         // TODO: Update your variables here
@@ -93,6 +98,51 @@ int main(void)
 
         EndDrawing();
 
+=======
+        if (currentScreen == MENU)
+        {
+            if (IsKeyPressed(KEY_DOWN)) selectedOption = (selectedOption + 1) % 3;
+            if (IsKeyPressed(KEY_UP)) selectedOption = (selectedOption - 1 + 3) % 3;
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                if (selectedOption == 0) currentScreen = GAME; // Iniciar juego
+                else if (selectedOption == 1) // Configuración
+                {
+                
+           
+                } 
+                else if (selectedOption == 2) 
+                {
+                    currentScreen = HTP;
+                }
+                else if (selectedOption == 3) break; // Salir
+            }
+
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawTexture(JotPKLogo, (GetScreenWidth() - JotPKLogo.width) / 2, 50, WHITE);
+            DrawText("Start", 190, 300, 20, selectedOption == 0 ? RED : WHITE);
+            DrawText("Settings", 190, 350, 20, selectedOption == 1 ? RED : WHITE);
+            DrawText("How to play", 190, 400, 20, selectedOption == 2 ? RED : WHITE);
+            DrawText("Quit", 190, 450, 20, selectedOption == 3 ? RED : WHITE);
+            EndDrawing();
+        }
+        else if (currentScreen == GAME)
+        {
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("Juego en progreso...", 190, 200, 20, WHITE);
+            EndDrawing();
+        }
+        else if (currentScreen == HTP) {
+            ClearBackground(BLACK);
+            BeginDrawing();
+            DrawText("HOW TO PLAY", GetScreenWidth() / 2, 50, WHITE);
+            DrawText("Sasdasdadassdad", 190, 300, 20, WHITE);
+
+            EndDrawing();
+        }
+>>>>>>> Stashed changes
     }
 
     // De-Initialization 
