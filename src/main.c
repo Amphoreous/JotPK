f93707d242729e8b09d0801efb3c158a0fbe09f1
TheUnrealZaka <info@theunrealzaka.me>
2025-02-17 08:33:05 +0100

    Dia 1

--- src/main.c
+++ src/main.c
@@ -1,18 +1,50 @@
 #include "raylib.h"
+#include "resource_dir.h"
 
-int main (void)
+typedef enum { MENU, GAME } GameScreen;
+
+int main(void)
 {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
-	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-	SetTargetFPS(60);
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-		EndDrawing();
-	}
-	CloseWindow();
-	return 0;
+    SetConfigFlags(FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_MAXIMIZED);
+    InitWindow(GetScreenWidth(), GetScreenHeight(), "Journey of the Prairie King");
+    SetTargetFPS(60);
+    SearchAndSetResourceDir("resources");
+    Texture JotPKLogo = LoadTexture("JotPK_logo.png");
+
+    GameScreen currentScreen = MENU;
+    int selectedOption = 0;
+
+    while (!WindowShouldClose())
+    {
+        if (currentScreen == MENU)
+        {
+            if (IsKeyPressed(KEY_DOWN)) selectedOption = (selectedOption + 1) % 3;
+            if (IsKeyPressed(KEY_UP)) selectedOption = (selectedOption - 1 + 3) % 3;
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                if (selectedOption == 0) currentScreen = GAME; // Iniciar juego
+                else if (selectedOption == 1) {} // Configuración
+                else if (selectedOption == 2) break; // Salir
+            }
+
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawTexture(JotPKLogo, (GetScreenWidth() - JotPKLogo.width) / 2, 50, WHITE);
+            DrawText("Iniciar Juego", 190, 300, 20, selectedOption == 0 ? RED : WHITE);
+            DrawText("Configuracion", 190, 350, 20, selectedOption == 1 ? RED : WHITE);
+            DrawText("Salir", 190, 400, 20, selectedOption == 2 ? RED : WHITE);
+            EndDrawing();
+        }
+        else if (currentScreen == GAME)
+        {
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("Juego en progreso...", 190, 200, 20, WHITE);
+            EndDrawing();
+        }
+    }
+
+    UnloadTexture(JotPKLogo);
+    CloseWindow();
+    return 0;
 }
